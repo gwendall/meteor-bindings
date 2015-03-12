c2229f6d72ae807a1fcccb990af5d6c5ba28a7ce
Gwen <gwendall.esnault@gmail.com>
2015-03-12 17:15:03 +0100

    first

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "gwendall:bindings",
   summary: "Easy two-way data-binding with Blaze",
   git: "https://github.com/gwendall/meteor-bindings.git",
-  version: "0.1.0"
+  version: "0.1.1"
 });
 
 Package.onUse(function (api, where) {
